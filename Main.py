d3eeebb300c9e0f9d9fc376271ca31c9a0ae159c
Brayden Dawson <bdaw211@mywhitecliffe.com>
2022-04-18 13:37:32 +1200

    Refactor test case methods in an order that suits. Add print statements to each test method. Ensure main file complies with PEP standard.

--- Main.py
+++ Main.py
@@ -1,36 +1,49 @@
 import unittest  # Import the testing framework
 import BowlingGame
 
+
 class TestBowlingGame(unittest.TestCase):
 
     def setUp(self):
         self.game = BowlingGame.BowlingGame()
 
-    def testGutterGame(self):
-        for i in range(0, 20):
-            self.game.rolls(0)
-        assert self.game.score()==0
-    def testAllOnes(self):
-        self.rollMany(1, 20)
-        assert self.game.score()==20
-    def testOneSpare(self):
-        self.game.rolls(5)
-        self.game.rolls(5)
-        self.game.rolls(3)
-        self.rollMany(0,17)
-        assert self.game.score()==16
     def testOneStrike(self):
+        print("Test case #1\n Testing game with one strike....\n")
         self.game.rolls(10)
         self.game.rolls(4)
         self.game.rolls(3)
-        self.rollMany(0,16)
-        assert  self.game.score()==24
-    def testPerfectGame(self):
-        self.rollMany(10,12)
-        assert self.game.score()==300
+        self.rollMany(0, 16)
+        assert self.game.score() == 24
+
     def testOneSpare(self):
-        self.rollMany(5,21)
-        assert self.game.score()==150
-    def rollMany(self, pins,rolls):
+        print("Test case #2\n Testing game with one spare....\n")
+        self.game.rolls(5)
+        self.game.rolls(5)
+        self.game.rolls(3)
+        self.rollMany(0, 17)
+        assert self.game.score() == 16
+
+    def testAllOnes(self):
+        print("Test case #3\n Testing game with each bowl hitting one pin....\n")
+        self.rollMany(1, 20)
+        assert self.game.score() == 20
+
+    def testGutterGame(self):
+        print("Test case #4\n Testing a gutter ball game....\n")
+        for i in range(0, 20):
+            self.game.rolls(0)
+        assert self.game.score() == 0
+
+    def testPerfectGame(self):
+        print("Test case #5\n Testing a perfect game....\n")
+        self.rollMany(10,12)
+        assert self.game.score() == 300
+
+    def testSpareGame(self):
+        print("Test case #6\n Testing a game with all spares....\n")
+        self.rollMany(5, 21)
+        assert self.game.score() == 150
+
+    def rollMany(self, pins, rolls):
         for i in range(rolls):
             self.game.rolls(pins)
