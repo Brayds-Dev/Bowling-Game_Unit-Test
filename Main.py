47253c548c180b41dde8380bb03db52803ad6bc0
Brayden Dawson <bdaw211@mywhitecliffe.com>
2022-04-18 14:05:35 +1200

    Rename functions to suit PEP standards ensure BowlingGame file adheres to PEP Standards

--- Main.py
+++ Main.py
@@ -4,46 +4,46 @@ import BowlingGame
 
 class TestBowlingGame(unittest.TestCase):
 
-    def setUp(self):
+    def set_up(self):
         self.game = BowlingGame.BowlingGame()
 
-    def testOneStrike(self):
+    def test_one_strike(self):
         print("Test case #1\n Testing game with one strike....\n")
         self.game.rolls(10)
         self.game.rolls(4)
         self.game.rolls(3)
-        self.rollMany(0, 16)
+        self.roll_many(0, 16)
         assert self.game.score() == 24
 
-    def testOneSpare(self):
+    def test_one_spare(self):
         print("Test case #2\n Testing game with one spare....\n")
         self.game.rolls(5)
         self.game.rolls(5)
         self.game.rolls(3)
-        self.rollMany(0, 17)
+        self.roll_many(0, 17)
         assert self.game.score() == 16
 
-    def testAllOnes(self):
+    def test_all_ones(self):
         print("Test case #3\n Testing game with each bowl hitting one pin....\n")
-        self.rollMany(1, 20)
+        self.roll_many(1, 20)
         assert self.game.score() == 20
 
-    def testGutterGame(self):
+    def test_gutter_game(self):
         print("Test case #4\n Testing a gutter ball game....\n")
         for i in range(0, 20):
             self.game.rolls(0)
         assert self.game.score() == 0
 
-    def testPerfectGame(self):
+    def test_perfect_game(self):
         print("Test case #5\n Testing a perfect game....\n")
-        self.rollMany(10,12)
+        self.roll_many(10, 12)
         assert self.game.score() == 300
 
-    def testSpareGame(self):
+    def test_spare_game(self):
         print("Test case #6\n Testing a game with all spares....\n")
-        self.rollMany(5, 21)
+        self.roll_many(5, 21)
         assert self.game.score() == 150
 
-    def rollMany(self, pins, rolls):
+    def roll_many(self, pins, rolls):
         for i in range(rolls):
             self.game.rolls(pins)
