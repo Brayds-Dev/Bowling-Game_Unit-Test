629743c5c08668f38a8c18fb6e84c996caa5441c
Brayden Dawson <bdaw211@mywhitecliffe.com>
2022-04-20 12:48:36 +1200

    Add doc strings to class and methods

--- BowlingGame.py
+++ BowlingGame.py
@@ -1,20 +1,68 @@
 class BowlingGame:
+    """
+    A class used to represent a bowling game
+
+    ...
+
+    Attributes
+    ----------
+    roll_index : int
+    pins : int
+    result : int
+
+    Methods
+    -------
+    roll(pins)
+        Adds the set number of pins to the rolls list
+
+    score
+        Adds up the score of the game using the game rules along with the type of score function
+        Returns the final score result
+
+    is_strike(roll_index)
+        method to check if strike is scored
+
+    is_spare(roll_index)
+        method to check if a spare is scored
+
+    strike_score(roll_index)
+        method to add a strike score rule
+
+    spare_score(roll_index)
+        method to add a spare score rule
+
+    frame_score(roll_index)
+        method to add a normal scored frame
+    """
     def __init__(self):
+        """
+        A constructor to initialise a new empty list for each game
+        """
         self.rolls = []
 
     def roll(self, pins):
+        """
+        A method to add the number of pins to the new list
+        :param pins:
+        :return:
+        """
         self.rolls.append(pins)
 
     def score(self):
+        """
+        A method to run through a full game and at each frame check what
+        score is to be added
+        :return: result
+        """
         result = 0
         roll_index = 0
 
-        for frameIndex in range(10):
+        for frameIndex in range(10):  # a loop that will run through 10 frames
 
             if self.is_strike(roll_index):
                 result += self.strike_score(roll_index)
                 roll_index += 1
-                # frameIndex += 1
+
             elif self.is_spare(roll_index):
                 result += self.spare_score(roll_index)
                 roll_index += 2
@@ -24,16 +72,42 @@ class BowlingGame:
         return result
 
     def is_strike(self, roll_index):
+        """
+        a method to check the given index == 10 therefore a strike
+        :param roll_index:
+        :return: true or false
+        """
         return self.rolls[roll_index] == 10
 
     def is_spare(self, roll_index):
+        """
+        a method to check the given index + the next == 10 therefore a spare
+        :param roll_index:
+        :return: true or false
+        """
         return self.rolls[roll_index] + self.rolls[roll_index+1] == 10
 
     def strike_score(self, roll_index):
+        """
+        a method that calculates a strikes total score based on the next two rolls
+        :param roll_index:
+        :return: int
+        """
         return 10 + self.rolls[roll_index+1] + self.rolls[roll_index+2]
 
     def spare_score(self, roll_index):
-        return 10 + self.rolls[roll_index+2]  # should potentially be +1
+        """
+        a method that calculates a spare total score based on the roll after the spare
+        :param roll_index:
+        :return: int
+        """
+        return 10 + self.rolls[roll_index+2]
 
     def frame_score(self, roll_index):
+        """
+        a method to calculate a standard frame when neither a spare nor strike is scored
+        the total of the two rolls to the frame
+        :param roll_index:
+        :return: int
+        """
         return self.rolls[roll_index] + self.rolls[roll_index + 1]
--- Main.py
+++ Main.py
@@ -3,16 +3,48 @@ import BowlingGame  # Import the BowlingGame class
 
 
 class TestBowlingGame(unittest.TestCase):
+    """
+    A class used to test instances of the bowling game class
+
+    ....
+
+    Methods
+    -------
+    setUp
+        Method to initialise an instance of the bowling game
+
+    test_one_strike
+        A method to test a game with one strike
+
+    test_one_spare
+        A method ti test a game with one spare
+
+    test_all_ones
+        A method to test a game where only 1 is scored on each roll
+
+    test_gutter_game
+        A method to test a game where no pins are scored
+
+    test_perfect game
+        A method to test a perfect score game (all strikes)
+
+    test_spare_game
+        A method to test a game filled with spares
+    """
 
     def setUp(self):
+        """
+        Hook method to set up the test instance before exercising it
+        :return:
+        """
         self.game = BowlingGame.BowlingGame()
 
     def test_one_strike(self):
         """
-        A method to test a game with one strike
+        A method to test a game with one strike with a test case assertion
         :return:
         """
-        print("Test case #1\n Testing game with one strike....\n")
+        print("Test case #1\n Testing game with one strike....")
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
@@ -20,7 +52,11 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score() == 24
 
     def test_one_spare(self):
-        print("Test case #2\n Testing game with one spare....\n")
+        """
+        A method to test a game with one spare followed by a test case assertion
+        :return:
+        """
+        print("Test case #2\n Testing game with one spare....")
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
@@ -28,26 +64,48 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score() == 16
 
     def test_all_ones(self):
-        print("Test case #3\n Testing game with each roll hitting one pin....\n")
+        """
+        A method to test a game where all rolls hit one pin followed by a test case assertion
+        :return:
+        """
+        print("Test case #3\n Testing game with each roll hitting one pin....")
         self.roll_many(1, 20)
         assert self.game.score() == 20
 
     def test_gutter_game(self):
-        print("Test case #4\n Testing a gutter ball game....\n")
+        """
+        A method to test a game where no pins are scored followed by a test case assertion
+        :return:
+        """
+        print("Test case #4\n Testing a gutter ball game....")
         for i in range(0, 20):
             self.game.roll(0)
         assert self.game.score() == 0
 
     def test_perfect_game(self):
-        print("Test case #5\n Testing a perfect game....\n")
+        """
+        A method to test a game where all rolls score a strike followed by a test assertion
+        :return:
+        """
+        print("Test case #5\n Testing a perfect game....")
         self.roll_many(10, 12)
         assert self.game.score() == 300
 
     def test_spare_game(self):
-        print("Test case #6\n Testing a game with all spares....\n")
+        """
+        A method to test a game where all rolls score a spare followed by a test assertion
+        :return:
+        """
+        print("Test case #6\n Testing a game with all spares....")
         self.roll_many(5, 21)
         assert self.game.score() == 150
 
     def roll_many(self, pins, rolls):
+        """
+        A method to add multiple rolls with a given score to the game
+        :param pins:
+        :param rolls:
+        :return:
+        """
         for i in range(rolls):
             self.game.roll(pins)
